5c221ce630bdf25a218473c0ffe3b80b6874072d
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-04-11 00:53:43 -0400

    Creamos la función en la que creamos el tablero y tenemos como entrada las columnas y las filas

--- autito.js
+++ autito.js
@@ -6,6 +6,15 @@ class Autito
     this.y = y;
     this.direccion=direccion;
   }
+
+  generarTablero(columnas, filas)
+  {
+    this.matriz = new Array(filas);
+    for (let i = 0; i < filas; i++) 
+    {
+        this.matriz[i] = new Array(columnas);
+    }
+  }
 }
 
 export default Autito;
