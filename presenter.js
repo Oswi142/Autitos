afc67f4cc8e33fa8674e1a08f413029a94146e09
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-04-11 02:53:42 -0400

    Hacemos cambios en el index y presenter

--- presenter.js
+++ presenter.js
@@ -1,20 +1,14 @@
 import Autito from "./autito.js";
 
+const div=document.querySelector("#divPosInicial");
 const labelPosFinal=document.querySelector("#posicion-final");
-const labelPosInicial=document.querySelector("#posicion-inicial");
-const labelComandos=document.querySelector("#comandos");
 const form=document.querySelector("#formulario");
-const posInicial=document.querySelector("#posicion-inicial")
 const comandos=document.querySelector("#comandos-input")
-const posFinal=document.querySelector("#posicion-final")
-let auto=new Auto(0,0,"N");
-
+let Auto=new Autito(0,0,"N");
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let cadena = comandos.value.split('/');
-  console.log(cadena);
-  labelPosInicial.innerHTML = cadena[1];
-
-  labelComandos.innerHTML = cadena[2];
-  labelPosFinal.innerHTML =  auto.ejecutarInstrucciones(comandos.value) ;
+event.preventDefault();
+div.innerHTML = "<p>" + auto.ejecutarInstrucciones("5,5/3,3E/AADAADADDA") + "</p>";
+let cadena = comandos.value.split('/');
+console.log(cadena);
+labelPosFinal.innerHTML = "<p>" + auto.ejecutarInstrucciones(comandos.value) + "</p>";
 });
