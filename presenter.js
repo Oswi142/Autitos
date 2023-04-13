dc9e8435470bd6c791819c2de2f9fc6e33a2a951
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-04-13 02:01:49 -0400

    Diseñando el index.html para que sea más vistoso

--- presenter.js
+++ presenter.js
@@ -1,20 +1,19 @@
-import Autito from "./autito.js";
+import Autito from "./autito";
 
-const labelPosFinal=document.querySelector("#posicion-final");
-const labelPosInicial=document.querySelector("#posicion-inicial");
+const form=document.querySelector("#form");
+const labelPosFinal=document.querySelector("#pos-final");
+const labelPosInicial=document.querySelector("#pos-inicial");
 const labelComandos=document.querySelector("#comandos");
-const form=document.querySelector("#formulario");
-const posInicial=document.querySelector("#posicion-inicial")
 const comandos=document.querySelector("#comandos-input")
-const posFinal=document.querySelector("#posicion-final")
-let Auto=new Autito(0,0,"N");
 
-form.addEventListener("submit", (event) => {
+let auto=new Autito(0,0,"N");
+
+form.addEventListener("submit", (event) => 
+{
   event.preventDefault();
   let cadena = comandos.value.split('/');
   console.log(cadena);
   labelPosInicial.innerHTML = cadena[1];
-
   labelComandos.innerHTML = cadena[2];
-  labelPosFinal.innerHTML =  Auto.ejecutarInstrucciones(comandos.value) ;
+  labelPosFinal.innerHTML =  auto.confirmarInstrucciones(comandos.value) ;
 });
